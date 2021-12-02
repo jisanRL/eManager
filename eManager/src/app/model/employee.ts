93720ccaf44918477afd516080add370d6e448f8
jisanRL <>
2021-12-02 16:15:47 -0500

    FrontEND:service and class is done, PUT method works

---
+++ eManager/src/app/model/employee.ts
@@ -0,0 +1,9 @@
+export class Employee {
+    id : number = 0;
+    name: String = "";
+    email : String = "";
+    jobTitle : String = "";
+    phone  : String = "";
+    imageURL : String = "";
+    employeeCode  : String = "";
+}
---
+++ eManager/src/app/service/http-client.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Employee } from '../model/employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpClientService {
+
+  constructor(private http: HttpClient) { }
+
+  // gets the employees
+  getEmployees() {
+    return this.http.get<Employee[]>('http://localhost:8080/user/employee');
+  }
+
+  // adds employee
+  addEmployees(employee: Employee) {
+    return this.http.post<Employee>('http://localhost:8080/user/add/', employee);
+  }
+
+  // updates the employee info
+  updateEmployees(employee: Employee) {
+    return this.http.put<Employee>('http://localhost:8080/user/update/', employee);
+  }
+
+  // deletes employee
+  deleteEmployee(id: number) {
+    return this.http.delete<Employee>('http://localhost:8080/user/' + id);
+  }
+
+  // search employee 
+  searchEmployee(name: String)  {
+    return this.http.get<Employee>('http://localhost:8080/user/get/' + name);
+  }
+  
+}
